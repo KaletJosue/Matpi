cd78ff8e9801b7e2bcd597845b3098658e68e1b0
KaletJosue <kalet.jo28@gmail.com>
2024-06-20 21:32:07 -0500

    cards

---
+++ controller/config/config.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    rest: true
+}) 
+
+sr.reveal('.textinicio',{delay:100, origin:'top'});
+sr.reveal('.imgscroll',{delay:300, origin:'top'});
+sr.reveal('.imgsmenu',{delay:50, origin:'left'});
+sr.reveal('.unocards',{delay:100, origin:'left'});
+sr.reveal('.doscards',{delay:100, origin:'left'});
+sr.reveal('.trescards',{delay:100, origin:'left'});
+sr.reveal('.cuatrocards',{delay:100, origin:'left'});
+sr.reveal('.cincocards',{delay:100, origin:'left'});
